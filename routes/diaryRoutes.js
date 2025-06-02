80ee17892a2af4cc43c995737d0af6c779aa1cab
Junseo Choi <chjwest01@gmail.com>
2025-06-02 20:27:40 +0900

    "일기 삭제 수정"

--- routes/diaryRoutes.js
+++ routes/diaryRoutes.js
@@ -654,7 +654,7 @@ router.delete('/deleteDiary', async (req, res) => {
       uid : req.query.uid,
       diaryDate : req.query.diaryDate
     });
-    if (result.deletedCount > 0 && invetionResult.deletedCount > 0){
+    if (invetionResult.deletedCount > 0 || result.deletedCount > 0){
       res.status(200).json({message: 'Diary Deleted'});
     }
     else{
